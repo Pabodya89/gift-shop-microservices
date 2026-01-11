5a365c82666af22cbe2c28a9aff65e71a4c3f439
Pabodya89 <2022cs209@stu.ucsc.cmb.ac.lk>
2026-01-11 22:52:43 +0530

    db file

---
+++ services/inventory-service/internal/database/db.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"log"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"services/inventory-service/internal/models"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := "root:@tcp(127.0.0.1:3306)/gift_shop_inventory?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	//Auto migrate tables
+	err = db.AutoMigrate(&models.Inventory{})
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+	}
+
+	DB = db
+	log.Println("Database connection established")
+}
