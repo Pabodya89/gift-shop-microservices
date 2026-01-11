0926512188a5ebd5984bcaf6b0df3edbd732cf0e
Pabodya89 <2022cs209@stu.ucsc.cmb.ac.lk>
2026-01-11 22:53:56 +0530

    repository file

--- services/inventory-service/internal/repository/item_repo.go
+++ services/inventory-service/internal/repository/item_repo.go
@@ -1,50 +1,49 @@
 package repository
 
-import "inventory-service/services/inventory-service/internal/model"
+import (
+	"services/inventory-service/internal/database"
+	"services/inventory-service/internal/model"
+	"gorm.io/gorm"
+)
 
-var items []model.Item
-
-var lastID int = 0
-
-func GetAllItems() []model.Item {
-	return items
+type ItemRepository interface {
+	CreateItem(item model.Item) (model.Item, error)
+	GetItemByID(id int) (model.Item, error)
+	UpdateItem(item model.Item) (model.Item, error)
+	DeleteItem(id int) error
 }
+type itemRepo struct {
+	db *gorm.DB
+}
+func NewItemRepository() ItemRepository {
+	return &itemRepo{
+		db: database.DB,
+	}
 
-func GetItemByID(id int) *model.Item {
-	for _,item := range items {
-		if item.ID == id {
-			return &item
-		}
+}
+func (r *itemRepo) CreateItem(item model.Item) (model.Item, error) {
+	if err := r.db.Create(&item).Error; err != nil {
+		return model.Item{}, err
+	}
+	return item, nil
+}
+func (r *itemRepo) GetItemByID(id int) (model.Item, error) {
+	var item model.Item
+	if err := r.db.First(&item, id).Error; err != nil {
+		return model.Item{}, err
+	}
+	return item, nil
+}
+func (r *itemRepo) UpdateItem(item model.Item) (model.Item, error) {
+	if err := r.db.Save(&item).Error; err != nil {
+		return model.Item{}, err
+	}
+	return item, nil
+}
+func (r *itemRepo) DeleteItem(id int) error {
+	if err := r.db.Delete(&model.Item{}, id).Error; err != nil {
+		return err
 	}
 	return nil
 }
 
-func CreateItem(item model.Item) model.Item {
-	lastID++
-	item.ID = lastID
-	items = append(items,item)
-	return item
-}
-
-func UpdateItem(id int, updated model.Item) *model.Item{
-	for i,item := range items {
-		if item.ID == id {
-			items[i].ID = updated.ID
-			items[i].Name = updated.Name
-			items[i].Price = updated.Price
-			items[i].Stock = updated.Stock
-			return &items[i]
-		}
-	}
-	return nil
-}
-
-func DeleteItem(id int) bool {
-	for i, item := range items {
-		if item.ID == id {
-			items = append(items[:i], items[:i+1]...)
-			return true
-		}
-	}
-	return false
-}
