8ee17a9f162c8d30a585122d65ac137b2f73e6dd
Pabodya89 <2022cs209@stu.ucsc.cmb.ac.lk>
2026-01-12 20:17:48 +0530

    order service file

---
+++ services/order-service/internal/service/order_service.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"github.com/gin-gonic/gin"
+	"services/order-service/internal/handlers"
+	"services/order-service/internal/routes"
+)
+
+type OrderService interface {
+	CreateOrder(order model.Order) (model.Order, error)
+	GetOrderByID(id int) (model.Order, error)
+	UpdateOrder(order model.Order) (model.Order, error)
+	DeleteOrder(id int) error
+}
+
+type orderService struct {
+	repo repository.OrderRepository
+}
+
+func NewOrderService(repo repository.OrderRepository) OrderService {
+	return &orderService{
+		repo: repo,
+	}
+}
+
+func (s *orderService) CreateOrder(order model.Order) (model.Order, error) {
+	if err := validator.ValidateOrder(order); err != nil {
+		return model.Order{}, err
+	}
+
+	
+	return s.repo.CreateOrder(order)
+}
+
+func (s *orderService) GetOrderByID(id int) (model.Order, error) {
+	return s.repo.GetOrderByID(id)
+}
+
+func (s *orderService) UpdateOrder(order model.Order) (model.Order, error) {
+	if err := validator.ValidateOrder(order); err != nil {
+		return model.Order{}, err
+	}
+	return s.repo.UpdateOrder(order)
+}
+
+func (s *orderService) DeleteOrder(id int) error {
+	return s.repo.DeleteOrder(id)
+}
+
+
+
+
+
+
